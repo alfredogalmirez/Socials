302108cb046a8414b6079d22da08beb18404885e
Alfredo Almirez <aalmirez2050@gmail.com>
2026-03-05 17:27:43 +0800

    Initialize Followers and Following in the Profile Page and etc.

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -9,17 +9,17 @@ class NotificationController extends Controller
     public function index()
     {
 
-        $notifications = auth()
-            ->user()
-            ->notifications()
-            ->with('actor')
-            ->latest()
-            ->get()
-            ->map(function ($notification) {
-                $data = $notification->toArray();
-                $data['created_at_human'] = $notification->created_at->diffForHumans();
-                return $data;
-            });
+        $notifications = auth()->user()
+        ->notifications()
+        ->with('actor')
+        ->latest()
+        ->get()
+        ->map(function ($notification) {
+            $data = $notification->toArray();
+            $data["created_at_human"] = $notification->created_at->diffForHumans();
+
+            return $data;
+        });
 
         return Inertia::render('Notifications/Index', [
             'notifications' => $notifications
